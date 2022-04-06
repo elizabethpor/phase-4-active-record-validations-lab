31ef2afe36512751f7192f55f1d3413cb4acd5f1
Elizabeth <elizabeth.por@gmail.com>
2022-04-06 09:39:42 -0500

    commit tree 081a5668a9b9715ea0a987ebcf8bcc8f964dc1f5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :includes_words
+
+    #custom validations
+
+    def includes_words
+        if [/Won't Believe | Secret | Guess | Top /].none? {|exp| exp.match(title)}
+            errors.add(:title, "needs to be more clickbait-y")
+        end
+    end
 end
